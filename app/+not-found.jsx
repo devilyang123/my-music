d8f776559d46765503b4ace93a347ff068c55bc7
devilyang123 <1695540137@qq.com>
2025-09-09 12:51:24 +0800

    Screen and player service

--- app/+not-found.jsx
+++ app/+not-found.jsx
@@ -1,14 +1,14 @@
-import { Link, Stack } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import {Link, Stack} from "expo-router";
+import {StyleSheet, View} from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
-      <View style={styles.container}>
-        <Link href="/">Go to home screen</Link>
-      </View>
-    </>
+      <>
+        <Stack.Screen options={{title: "Oops! This screen doesn't exist."}}/>
+        <View style={styles.container}>
+          <Link href="/">Go to home screen</Link>
+        </View>
+      </>
   );
 }
 const styles = StyleSheet.create({
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,21 @@
-import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import {Stack} from "expo-router";
+import {PaperProvider} from "react-native-paper";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import TrackPlayer from 'react-native-track-player';
+
+// register player service
+TrackPlayer.registerPlaybackService(() => require('../config/PlaybackService'));
+
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <PaperProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </PaperProvider>
-    </SafeAreaProvider>
+      <SafeAreaProvider>
+        <PaperProvider>
+          <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="(tabs)"/>
+            <Stack.Screen name="musicList"/>
+          </Stack>
+        </PaperProvider>
+      </SafeAreaProvider>
   );
 }
