1fa99d59c7cdf83a3d79ff22edd5273b9149cd24
devilyang123 <1695540137@qq.com>
2025-09-09 12:50:13 +0800

    Tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,48 +1,48 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {Tabs} from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="album" color={color} size={28} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="music"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="folder-music-outline"
-              color={color}
-              size={28}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="cog-outline"
-              color={color}
-              size={28}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      <Tabs
+          screenOptions={{
+            headerShown: false,
+          }}
+      >
+        <Tabs.Screen
+            name="index"
+            options={{
+              title: "",
+              tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons name="album" color={color} size={28}/>
+              ),
+            }}
+        />
+        <Tabs.Screen
+            name="music"
+            options={{
+              title: "",
+              tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons
+                      name="folder-music-outline"
+                      color={color}
+                      size={28}
+                  />
+              ),
+            }}
+        />
+        <Tabs.Screen
+            name="settings"
+            options={{
+              title: "",
+              tabBarIcon: ({color, size}) => (
+                  <MaterialCommunityIcons
+                      name="cog-outline"
+                      color={color}
+                      size={28}
+                  />
+              ),
+            }}
+        />
+      </Tabs>
   );
 }
